4d5beea2bf3a178d5f50c3f8679c5efaa510ee09
Lamharzi Alaoui Rabii <103124512+RabiiAlaouiLamharzi@users.noreply.github.com>
2024-10-25 21:16:09 +0200

    Updated TouchpadMouvement.cs

--- Assets/TouchpadMouvement.cs
+++ Assets/TouchpadMouvement.cs
@@ -6,11 +6,11 @@ using UnityEngine.XR;
 
 public class MovementProvider : LocomotionSystem
 {
-    public float speed = 2.0f;                     // Speed of the movement
-    public float gravityMultiplier = 1.0f;         // Multiplier for gravity effect
-    public List<XRController> controllers = null;  // List of XR controllers
-    private CharacterController characterController; // CharacterController component
-    private GameObject head;                        // Reference to the camera GameObject
+    public float speed = 2.0f;                     
+    public float gravityMultiplier = 1.0f;         
+    public List<XRController> controllers = null; 
+    private CharacterController characterController; 
+    private GameObject head;                       
 
     protected void Awake()
     {
@@ -25,7 +25,6 @@ public class MovementProvider : LocomotionSystem
 
     private void PositionController()
     {
-        // Adjust the height and center of the CharacterController based on the head position
         float headHeight = Mathf.Clamp(head.transform.localPosition.y, 1, 2);
         characterController.height = headHeight;
         Vector3 newCenter = new Vector3(head.transform.localPosition.x, headHeight / 2 + characterController.skinWidth, head.transform.localPosition.z);
@@ -62,18 +61,15 @@ public class MovementProvider : LocomotionSystem
     private void StartMove(Vector2 position)
     {
         Vector3 direction = new Vector3(position.x, 0, position.y);
-        // Rotate the movement direction based on the camera's yaw (head's rotation)
         Vector3 headRotation = new Vector3(0, head.transform.eulerAngles.y, 0);
         direction = Quaternion.Euler(headRotation) * direction;
         Vector3 movement = direction * speed;
 
-        // Move the character controller
         characterController.Move(movement * Time.deltaTime);
     }
 
     private void ApplyGravity()
     {
-        // Apply gravity to the character
         Vector3 gravity = new Vector3(0, Physics.gravity.y * gravityMultiplier, 0);
         characterController.Move(gravity * Time.deltaTime);
     }
